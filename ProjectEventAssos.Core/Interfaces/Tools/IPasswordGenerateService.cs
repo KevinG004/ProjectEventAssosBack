773449b1efb3b6ac3d9782063c4e2e413064c9bf
shrek206 <kevin.grignard.pro@gmail.com>
2026-03-27 14:05:18 +0100

    Hashage mdp et mdp généré aléatoirement

---
+++ ProjectEventAssos.Core/Interfaces/Tools/IPasswordGenerateService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectEventAssos.Core.Interfaces.Tools
+{
+    public interface IPasswordGenerateService
+    {
+        public string GeneratePassword();
+    }
+}
