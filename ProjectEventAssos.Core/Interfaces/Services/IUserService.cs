8799ee0eac1400820b485f31b99ebc8b743861fc
Luca Giglio <Lucagiglio2507@gmail.com>
2026-03-30 11:02:22 +0200

    Service+Repo (vérification a faire sérieusement)

---
+++ ProjectEventAssos.Core/Interfaces/Services/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using ProjectEventAssos.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectEventAssos.Core.Interfaces.Services
+{
+    public interface IUserService : IBaseService<User, Guid>
+    {
+        Task<User?> GetUserByEmail(string email);
+    }
+}
---
+++ ProjectEventAssos.Core/Services/UserService.cs
@@ -0,0 +1,56 @@
+﻿using ProjectEventAssos.Core.Interfaces.Repositories;
+using ProjectEventAssos.Core.Interfaces.Services;
+using ProjectEventAssos.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace ProjectEventAssos.SecurityTools.Services
+{
+    public class UserService(IUserRepository _userRespository) : IUserService
+    {
+        public Task<User> AddAsync(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> CountAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var existingUser = await _userRespository.ExistsAsync(id);
+            if (!existingUser) throw new KeyNotFoundException("Id not found");
+            await _userRespository.DeleteAsync(id);
+        }
+
+        public Task<IEnumerable<User>> FindAsync(Expression<Func<User, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<User>> GetAllAsync()
+        {
+            return await _userRespository.GetAllAsync();
+        }
+
+        public async Task<User?> GetByIdAsync(Guid id)
+        {
+            return await _userRespository.GetByIdAsync(id);
+            throw new NotImplementedException();
+        }
+
+        public Task<User?> GetUserByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateAsync(Guid id, User user)
+        {
+            await _userRespository.UpdateAsync(id, user);
+        }
+    }
+}
